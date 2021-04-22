b89b392aa46f65091304865ca22439aa6f04785e
lsarrazi <lsarrazi@student.42.fr>
2021-04-22 07:19:15 +0200

    set test

--- tests/float_register.cpp
+++ tests/float_register.cpp
@@ -3,7 +3,12 @@
 
 int main()
 {
-	Float f(1.4);
+	Float a(256, 1.4);
+	Float b(256, 2.6);
 
-	
+	FloatRegister& r = a + b + b + a;
+
+	std::cout << "output:" << r.toString() << std::endl;
+
+	FloatRegister::clean();
 }
